fd492a21dcd6b23e8e995d9778672f8bbf185854
cdiazcoder <cdiazcoder@gmail.com>
2026-04-27 23:58:56 +0200

    cors added

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    CORS_ALLOWED_ORIGINS: str = "http://127.0.0.1:5173,http://localhost:5173,https://creciendo-git-main-cdiazcod17s-projects.vercel.app,https://creciendo-jlh8vwwop-cdiazcod17s-projects.vercel.app"
+    CORS_ALLOWED_ORIGINS: str = "http://127.0.0.1:5173,http://localhost:5173,https://creciendo-git-main-cdiazcod17s-projects.vercel.app,https://creciendo-jlh8vwwop-cdiazcod17s-projects.vercel.app,https://creciendo-azure.vercel.app"
     DATABASE_URL: str
 
     model_config = SettingsConfigDict(
